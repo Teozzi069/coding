5ac71fe33af080cc1cc4ec7e1c50fa9124abb5f3
Teozzi069 <matteo.canale08@gmail.com>
2025-02-17 19:09:09 +0000

    fsgsdg

--- lezione2/guess_the_number.py
+++ lezione2/guess_the_number.py
@@ -1,10 +1,10 @@
 import random
 n=random.randint(1,100)
-tentativi=
-tentativi=tentativi+1
+tentativi=0
 win=False
 while not win:
     guess=int(input("prova a indovinare:"))
+    tentativi=tentativi+1
     if guess==n:
         win=True
         print("hai vinto con",tentativi,"tentativi!")
--- lezione2/soluzioni/ddos.py
+++
@@ -1,5 +0,0 @@
-import requests
-target=input()
-while True:
-    r=rquests.get(target)
-    print(r.status_code)
